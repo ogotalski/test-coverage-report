6fdfa056e71e6adda7b3d70cc5e87eacfd224c74
Aleh Hatalski <aleh.hatalski@wolt.com>
2022-07-25 22:42:36 +0200

    Fix workflow

--- __tests__/test.js
+++ __tests__/test.js
@@ -1,7 +1,7 @@
 
 const core = require("@actions/core");
 const github = require("@actions/github");
-const action = require("../src/action");
+const action = require("../action");
 jest.mock("@actions/core");
 jest.mock("@actions/github");
 
